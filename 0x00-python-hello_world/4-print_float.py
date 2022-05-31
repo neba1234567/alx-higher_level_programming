e1b7d96c7ae54cb67343a556c015e9f224bca97b
Nebil <nebilabdu300@gmail.com>
2022-05-31 14:14:41 -0400

    add all work

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 number = 3.14159
-x = round(number,2)
-print(f'{number} {x}')
+print(f"Float: {number:.2f}")
--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print(f'{str}' * 3 + '\n')
-print(str[:10])
+print("{}{}{}\n{}".format(str, str, str, str[0:9]))
