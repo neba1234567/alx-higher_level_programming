c61cf48a68190712cdcb30f0e910b218da0552ba
Nebil <nebilabdu300@gmail.com>
2022-06-02 15:46:24 -0400

    just another dose

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-
-
 if __name__ == "__main__":
-    """
-    Handle basic arithmetic operations.
-    """
-    from calculator_1 import add, sub, mul, div
     import sys
-
-    if len(sys.argv) - 1 != 3:
+    from calculator_1 import add, sub, mul, div
+    args = sys.argv
+    if len(args) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
+    if args[2] != '+' and args[2] != '-' and args[2] != '*' and args[2] != '/':
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+        exit(1)
+    a = int(args[1])
+    b = int(args[3])
+    if args[2] == '+':
+        print("{} {} {} = {}".format(a, args[2], b, add(a, b)))
+    elif args[2] == '-':
+        print("{} {} {} = {}".format(a, args[2], b, sub(a, b)))
+    elif args[2] == '*':
+        print("{} {} {} = {}".format(a, args[2], b, mul(a, b)))
+    elif args[2] == '/':
+        print("{} {} {} = {}".format(a, args[2], b, div(a, b)))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-
-
 if __name__ == "__main__":
-    """
-    Print all names defined by hidden_4 module.
-    """
     import hidden_4
-
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    temp = dir(hidden_4)
+    for i in range(len(temp)):
+        for j in range(len(temp[i])):
+            if (temp[i][j] == '_' and temp[i][j+1] == '_'):
+                break
+            else:
+                print(temp[i])
+                break
