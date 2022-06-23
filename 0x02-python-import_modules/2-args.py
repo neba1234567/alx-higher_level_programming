1d097a14b140a140d15c259a8b063e72622d521a
Nebil <nebilabdu300@gmail.com>
2022-06-23 13:30:25 -0400

    checking my work

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
         print("{:d} argumnets.".format(i - 1))
     elif i == 2:
         print("{:d} argument:".format(i - 1))
-        print("{:d}: {}".format(i -1, sys.argv[1]))
+        print("{:d}: {}".format(i - 1, sys.argv[1]))
     elif i > 2:
         print("{:d} arguments:".format(i - 1))
         for x in range(1, i):
