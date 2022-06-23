73b0109a3e27a62aa7b215046cbc4c31cbf36cd5
Nebil <nebilabdu300@gmail.com>
2022-06-23 13:51:03 -0400

    checking my work

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     i = len(sys.argv)
     if i == 1:
-        print("{:d} argumnets.".format(i - 1))
+        print("{:d} arguments.".format(i - 1))
     elif i == 2:
         print("{:d} argument:".format(i - 1))
         print("{:d}: {}".format(i - 1, sys.argv[1]))
