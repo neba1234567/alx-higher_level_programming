b7f9ad01a3771653cb673b09c70f92e8acb958b3
Nebil <nebilabdu300@gmail.com>
2022-07-04 03:09:16 -0400

    fix my way

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if idx < 0:
-        return None
+        return my_list
     elif idx > len(my_list):
-        return None
+        return my_list
     else:
-	my_list.pop(idx)
-	my_list.insert(idx, element)
+        my_list[idx] = element
+        new_list = my_list
         return my_list
