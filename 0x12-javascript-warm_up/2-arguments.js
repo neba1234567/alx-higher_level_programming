98186d41c7fecf741afb8e0b784f5bc8214e59c1
Nebil Abdulfetah <nebilabdulfetah33@gmail.com>
2022-09-08 11:04:23 -0400

    just trying

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const count = process.argv.length;
+if (count === 2)
+{
+  console.log("No argument");
+}
+if (count === 3)
+{
+  console.log("Argument found");
+}
+if (count > 3)
+{
+  console.log("Arguments found");
+}
